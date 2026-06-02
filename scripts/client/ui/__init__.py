47acc987ee257e4c4f57b2bcab114e5c19f06e59
Anterior <50525835+enuquak@users.noreply.github.com>
2026-06-02 22:48:59 +0800

    feat: update UI __init__.py to export notification modules

--- scripts/client/ui/__init__.py
+++ scripts/client/ui/__init__.py
@@ -1,4 +1,27 @@
 """
 UI 模块
-包含快捷栏、背包面板等 UI 组件
+包含游戏内所有 UI 组件
 """
+from .energy_bar import EnergyBar
+from .exhaustion_modal import ExhaustionModal
+from .hotbar import Hotbar
+from .inventory_panel import InventoryPanel
+from .time_hud import TimeHUD
+from .notification_manager import NotificationManager
+from .toast_channel import ToastChannel
+from .marquee_channel import MarqueeChannel
+from .toast_renderer import ToastRenderer
+from .marquee_renderer import MarqueeRenderer
+
+__all__ = [
+    'EnergyBar',
+    'ExhaustionModal',
+    'Hotbar',
+    'InventoryPanel',
+    'TimeHUD',
+    'NotificationManager',
+    'ToastChannel',
+    'MarqueeChannel',
+    'ToastRenderer',
+    'MarqueeRenderer',
+]
